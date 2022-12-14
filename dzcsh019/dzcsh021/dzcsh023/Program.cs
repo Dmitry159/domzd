0849dd0466898d7f0e69880d77a51c23a700f0c8
Dmitry <godivl@mail.ru>
2022-12-14 17:00:29 +0500

    dzsh023

---
+++ dzcsh019/dzcsh021/dzcsh023/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Задача№ 23 на входе N на выходе тадлица кубов чисел
+//3->1.8.27
+//5->1.8.27.125
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+    int i = 1;
+    int M =1;
+    while (N >=i)
+    { 
+        M=i*i*i;
+        i++;
+        Console.WriteLine( M );
+    }
+    return i;
