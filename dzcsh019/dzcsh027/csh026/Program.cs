1f8f43c6e3164ebaec723113f0c9e83330ea135a
Dmitry <godivl@mail.ru>
2022-12-18 21:55:14 +0500

    csh026

---
+++ dzcsh019/dzcsh027/csh026/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Задача 26: принимает число выдает количество цифр в числе
+//456-3
+
+Console.WriteLine("Введите число : ");
+int number = int.Parse(Console.ReadLine()!);
+
+int GetCountNuw(int num)
+{
+    int i = 0;
+    while (number > 0) 
+    {
+        number = number / 10;
+        i++;
+    }
+    return i;
+}
+Console.WriteLine($"Колличество чисел = {GetCountNuw(number)}");
