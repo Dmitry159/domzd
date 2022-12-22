1818e5b71e27340dda535855b09f20b025e702e6
Dmitry <godivl@mail.ru>
2022-12-22 18:21:37 +0500

    dzcsh036

---
+++ dzcsg036/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+void FillArray(int[] numb)
+{
+    int arr = numb.Length;
+    int i = 0;
+    while (i < arr)
+    {
+        numb[i] = new Random().Next(0, 100);
+        i++;
+    }
+}
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int sum=0;
+    int j = 0;
+    for (j=0;j < count;j++)
+    {
+        
+        
+        Console.Write($"\t{array[j]}");
+       
+           if(j%2==0)
+        {
+            sum=array[j]+sum;
+        }
+        
+        
+    }
+    Console.WriteLine( -(sum));
+}
+int[] array = new int[4];
+
+FillArray(array);
+PrintArray(array);
