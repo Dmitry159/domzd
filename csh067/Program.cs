9322496980bb46a206ce732270fc1f0c7fa23c50
Dmitry <godivl@mail.ru>
2022-12-26 00:12:07 +0500

    csh067

---
+++ csh067/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Задача №67 сумма цифр в числе.
+System.Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int SumNumber(int num)
+{
+    if (num ==0)
+    {
+        return 0;
+    }
+    return (num % 10 + SumNumber(num / 10) );
+}
+
+System.Console.WriteLine(SumNumber(N));
