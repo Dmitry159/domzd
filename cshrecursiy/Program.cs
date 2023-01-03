d1b7ec058fbba48841760c019f3fc65be6f2361f
Dmitry <godivl@mail.ru>
2023-01-03 19:59:39 +0500

    recursiy

---
+++ cshrecursiy/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+string NumbersFor(int a, int b)
+{
+    string result = String.Empty;
+    for (int i=a; i <=b; i++)//i>=b;i-- в обратную сторону
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string NumbersRec(int a , int b)
+{
+    if(a <= b)return NumbersRec(a+1,b) + $"{a} ";
+   else return String.Empty;
+    }
+
+Console.WriteLine(NumbersFor(1,10));
+Console.WriteLine(NumbersRec(1,10));
