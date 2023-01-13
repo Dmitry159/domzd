f5df70ec6934eb6dc9c3737ea28c4f045cf7894b
Dmitry <godivl@mail.ru>
2023-01-13 20:48:43 +0500

    csh030

---
+++ csh030/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Задача 30 Прогр создает массив
+
+int N=8;
+int [] arr=new int[N];
+void Getarray(int[]array)
+{
+    
+    for(int i=0;i<N ;i++)
+    {
+        array[i]=new Random().Next(0,100);
+    
+        Console.Write($"{array[i]}");
+        i++;
+    }
+}
+Getarray(arr);
