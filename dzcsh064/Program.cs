57f45c7b1b8bb7fd4da15d2a95f70992af16f2b6
Dmitry <godivl@mail.ru>
2023-01-14 20:51:46 +0500

    dzcsh064

---
+++ dzcsh064/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Задача №64 Задайте N. Напишите программу которая выведет все натуральные числа от N до 1
+//Выполнить с помошью рекурсии
+
+Console.WriteLine("Введите число N : ");
+double num = int.Parse(Console.ReadLine()!);
+
+double FulRec (double n)
+{
+    if (n==1) return 1;
+    else  Console.Write(n );
+    n=n-1;
+    return  FulRec(n);
+     
+      }
+           
+      Console.WriteLine(FulRec(num));
+
