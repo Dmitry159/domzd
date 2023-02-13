6d9b98bd6b6e441f867648334e722c971a6a89fd
Dmitry <godivl@mail.ru>
2023-02-13 18:01:14 +0500

    itogovaydz

---
+++ itogovay1blok/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите элементы массива через пробел: ");
+string elements=Console.ReadLine();
+string[] baseArray=GetArrayFromString(elements);
+
+//Console.WriteLine(String.Join(" ",res(baseArray)));//выводит массив
+
+string[]GetArrayFromString(string stringArray)
+{
+    string[]nums=stringArray.Split();//собирает строку в массив
+    string[]res=new string[nums.Length];
+    for(int i=0;i<nums.Length;i++)
+    {
+        res[i]=nums[i];
+    }
+    return res;
+}
+
+string[]strings = baseArray;
+var result = strings.Where(strings => strings.Length <=3).ToArray();
+Console.WriteLine(string.Join(" , ", result));
